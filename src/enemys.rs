2333fd0305de1ec895d7342fb98f9033b351ea41
coHipa <96304612+coHipa@users.noreply.github.com>
2023-12-20 22:57:46 +0100

    Try to figure out how to use the loops correctly

--- src/enemys.rs
+++ src/enemys.rs
@@ -1,6 +1,5 @@
 use std::cmp::Ordering;
 use::std::io;
-use random_number::random;
 pub struct Player {
     pub hp: u8,
     pub dmg: u8,
@@ -9,17 +8,16 @@ pub struct Player {
 pub struct Enemy {
     pub hp: u8,
     pub dmg: u8,
+    pub sec_num: u8,
 }
 
 pub fn attack_little(player: &mut Player, enemy: &mut Enemy) {
-    let secret_number: u8 = random!(0, 10);
-
     println!("Attack with your guess (0-10)");
     let mut player_input = String::new();
     io::stdin().read_line(&mut player_input).expect("Invalid Input");
     let player_guess: u8 = player_input.trim().parse().expect("You must enter a number");
 
-    if player_guess == secret_number {
+    if player_guess == enemy.sec_num {
         enemy.hp -= player.dmg;
         println!("You killed that enemy.");
     } else {
@@ -28,26 +26,27 @@ pub fn attack_little(player: &mut Player, enemy: &mut Enemy) {
     }
 }
 
-pub fn attack_boss(player: &mut Player, enemy: &mut Enemy) {
-    let secret_number: u8 = random!(0, 100);
-
+pub fn attack_boss(player: &mut Player, enemy: &mut Enemy) -> bool {
     println!("Attack with your guess (0-100)");
     let mut player_input = String::new();
     io::stdin().read_line(&mut player_input).expect("Invalid Input");
     let player_guess: u8 = player_input.trim().parse().expect("You must enter a number");
 
-    match player_guess.cmp(&secret_number) {
+    match player_guess.cmp(&enemy.sec_num) {
         Ordering::Greater => {
             player.hp -= enemy.dmg;
-            println!("You guessed high, your HP: {}", player.hp)
+            println!("You guessed high, your HP: {}", player.hp);
+            return false;
         }
         Ordering::Less => {
             player.hp -= enemy.dmg;
-            println!("You guessed low, your HP: {}", player.hp)
+            println!("You guessed low, your HP: {}", player.hp);
+            return false;
         }
         Ordering::Equal => {
             enemy.hp -= player.dmg;
             println!("Your guess was right, boss HP: {}", enemy.hp);
+            return true;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use crate::enemys::{Player, Enemy, attack_little, attack_boss};
 mod enemys;
 fn main() {
     println!("Guess through hell");
+    println!("You wake up in hell and have to guess your way out.");
 
     let mut player = Player {hp: 100, dmg: 10};
     
     while player.hp > 0 {
         let enemy_count = random!(1,5);
-        let mut boss_enemy = Enemy {hp: 100, dmg: 20};
+        let mut little_enemy = Enemy {hp: 10, dmg: 5, sec_num: random!(1, 10)};
+        let mut boss_enemy = Enemy {hp: 100, dmg: 20, sec_num: random!(1, 100)};
+        let mut is_dead = false;
         
-        for i in 1..=enemy_count {
-            let mut little_enemy = Enemy {hp: 10, dmg: 5};
-           
+        for _i in 1..=enemy_count {   
             attack_little(&mut player, &mut little_enemy)
         }
-
-        attack_boss(&mut player, &mut boss_enemy)
+        while !is_dead{
+            is_dead = attack_boss(&mut player, &mut boss_enemy); 
+        }
     }
 }
